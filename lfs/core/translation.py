976de73a12d67ac9ef5220e68a0504b956c4267f
Pigletto <pigletto@gmail.com>
2013-03-03 22:29:33 +0100

    updated translations

--- lfs/core/translation.py
+++ lfs/core/translation.py
@@ -1,19 +1,21 @@
 try:
     from modeltranslation.translator import translator, TranslationOptions
-    from .models import Action, Country, Shop
-
+    from .models import Action, Country, Shop, ActionGroup
 
     class ActionTranslationOptions(TranslationOptions):
         fields = ('title', 'link')
-
     translator.register(Action, ActionTranslationOptions)
 
     class CountryTranslationOptions(TranslationOptions):
         fields = ('name', )
+    translator.register(Country, CountryTranslationOptions)
 
     class ShopTranslationOptions(TranslationOptions):
         fields = ('name', 'description', 'image', 'meta_title', 'meta_keywords', 'meta_description')
-
     translator.register(Shop, ShopTranslationOptions)
+
+    class ActionGroupTranslationOptions(TranslationOptions):
+        fields = ('name', )
+    translator.register(ActionGroup, ActionGroupTranslationOptions)
 except ImportError:
     pass
