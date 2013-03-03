4de8899b5277ea129c1d03cc8e46802f991f3df3
Pigletto <pigletto@gmail.com>
2013-03-03 22:52:04 +0100

    admin integration

--- lfs/core/admin.py
+++ lfs/core/admin.py
@@ -1,11 +1,22 @@
 # django imports
 from django.contrib import admin
 
+from modeltranslation.admin import TranslationAdmin
+
 # lfs imports
 from lfs.core.models import Action
 from lfs.core.models import ActionGroup
 from lfs.core.models import Shop
 from lfs.core.models import Country
+
+
+class ActionGroupAdmin(TranslationAdmin):
+    pass
+
+class CountryAdmin(TranslationAdmin):
+    pass
+
+admin.site.register(Country, CountryAdmin)
 
 admin.site.register(Shop)
 admin.site.register(Action)
