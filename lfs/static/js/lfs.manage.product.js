71982d3d6173b83393be43d4b5be80f0f2fc3f34
Jakub Wiśniowski <restless.being@gmail.com>
2014-11-29 23:53:52 +0100

    refactored management panel to work with recent js libs

--- lfs/static/js/lfs.manage.product.js
+++ lfs/static/js/lfs.manage.product.js
@@ -43,30 +43,83 @@ $(function() {
         }
     });
 
-    $('#file_upload').fileUploadUI({
-        uploadTable: $('#files'),
-        multiFileRequest : true,
-        buildUploadRow: function (files) {
+    // images tab
+    var image_file_upload = $('#file_upload'),
+        image_upload_url = image_file_upload.prop('action');
+
+    image_file_upload.fileupload({
+        url: image_upload_url,
+        dataType: 'json',
+        singleFileUploads: false // add event will now receive a list of files
+    }).on('fileuploadadd', function (e, data) {
+            var files_table = $("#images-uploads-table");
             var fileNames = '';
-            for (i = 0; i < files.length; i += 1) {
-                fileNames = fileNames + files[i].name + '<br>';
-            }
-            var msg = $("#files").attr("msg");
-            return $(
+            var msg = files_table.attr("msg");
+
+            $.each(data.files, function (index, file) {
+                fileNames = fileNames + file.name + '<br>';
+            });
+            files_table.append($(
                 '<tr>' +
                 '<td><div style="font-weight:bold; padding-bottom:10px">' + msg + '<img src="' + STATIC_URL + 'img/ajax-loader.gif" style="padding:8px 0 0 10px" /></div>' + fileNames + '<\/td>' +
                 '<\/tr>'
-            );
-        },
-        onLoadAll: function(files) {
-            var url = $("#files").attr("data");
+            ));
+
+            data.submit();
+      }).on('fileuploaddone', function (e, data) {
+            var uploads_table = $("#images-uploads-table");
+            var url = uploads_table.attr("data");
+
+            // after upload is done remove information about uploading
+            uploads_table.empty();
+
             $.get(url, function(data) {
                 data = safeParseJSON(data);
-                $("#images").html(data["images"]);
+                $("#images-list").html(data["images"]);
                 $.jGrowl(data["message"]);
             });
-        }
-    });
+      })
+    .prop('disabled', !$.support.fileInput)
+    .parent().addClass($.support.fileInput ? undefined : 'disabled');
+
+    // attachments tab
+    var attachment_upload = $('#attachment_upload'),
+        attachment_upload_url = attachment_upload.prop('action');
+
+    attachment_upload.fileupload({
+        url: attachment_upload_url,
+        dataType: 'json',
+        singleFileUploads: false // add event will now receive a list of files
+    }).on('fileuploadadd', function (e, data) {
+            var files_table = $("#attachment-uploads-table");
+            var fileNames = '';
+            var msg = files_table.attr("msg");
+
+            $.each(data.files, function (index, file) {
+                fileNames = fileNames + file.name + '<br>';
+            });
+            files_table.append($(
+                '<tr>' +
+                '<td><div style="font-weight:bold; padding-bottom:10px">' + msg + '<img src="' + STATIC_URL + 'img/ajax-loader.gif" style="padding:8px 0 0 10px" /></div>' + fileNames + '<\/td>' +
+                '<\/tr>'
+            ));
+
+            data.submit();
+      }).on('fileuploaddone', function (e, data) {
+            var uploads_table = $("#attachment-uploads-table");
+            var url = uploads_table.attr("data");
+
+            // after upload is done remove information about uploading
+            uploads_table.empty();
+            $.get(url, function(data) {
+                data = safeParseJSON(data);
+                $("#attachments-list").html(data["html"]);
+                $.jGrowl(data["message"]);
+            });
+      })
+    .prop('disabled', !$.support.fileInput)
+    .parent().addClass($.support.fileInput ? undefined : 'disabled');
+
 
     $body.on('submit', '#imagebrowser-filter', function(){
         $(this).ajaxSubmit({success: function(data) {
